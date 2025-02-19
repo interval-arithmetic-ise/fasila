14f88949ccde1710b2455b88e247c4234f6922e7
Berkay Dinç <berkayydinc@gmail.com>
2025-02-19 16:57:16 +0300

    Use intrinsics with core library

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
+#![feature(riscv_ext_intrinsics)]
+
 #![no_std]
 #![no_main]
 
-#![feature(link_llvm_intrinsics)]
-#![feature(abi_unadjusted)]
-
 use core::panic::PanicInfo;
-
-extern "unadjusted" {
-    #[link_name = "llvm.riscv.intadd.s"]
-    fn intadd_s(a: f64, b: f64) -> f64;
-}
-
-pub unsafe fn __intadd_s(a: f64, b: f64) -> f64 {
-    intadd_s(a, b)
-}
+use core::arch::riscv64::intadd_s;
 
 #[no_mangle]
 fn _start() -> ! {
-    let a = core::hint::black_box(0.007812505573383532); // [-1.0, 1.0]
-    let b = core::hint::black_box(2.0000014305114746); // [-2.0, 2.0]
-    let c = unsafe { core::hint::black_box(__intadd_s(a, b)) };
-    core::hint::black_box((a, b, c));
+    let a = 0.007812505573383532; // [-1.0, 1.0]
+    let b = 2.0000014305114746;   // [-2.0, 2.0]
+    let c = unsafe { intadd_s(a, b) };
+    core::hint::black_box(c);
     unsafe { riscv::asm::ebreak() };
     loop { }
 }
